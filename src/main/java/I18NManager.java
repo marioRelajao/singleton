6966c7c8668630bed74dc7012c2f97fd11bc226c
marioRelajao <mario.cerda@estudiantat.upc.edu>
2022-11-30 17:49:44 +0100

    1st Approach - Added Manager, classes & resources

---
+++ src/main/java/I18NManager.java
@@ -0,0 +1,16 @@
+public class I18NManager {
+    private  static I18NManager ourInstance = new I18NManager();
+
+    public static I18NManager getInstance(){
+        return ourInstance;
+    }
+
+    private I18NManager(){};
+
+    public void clear() {
+    }
+
+    public String getText(String idioma, String key) {
+
+    }
+}
